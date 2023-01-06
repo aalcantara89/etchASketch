6d8261f81c942c39a2f5fb190eac366d0e894f57
Anibal <aalcantara89@gmail.com>
2023-01-05 19:58:29 -0500

    updated game to add all requirements

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const container = document.getElementById("container");
-const btn = document.querySelectorAll("button");
-btn.addEventListener('click', () =>{
+const btn = document.querySelectorAll("#button");
+createGrid(16, 16);
+//btn.addEventListener('click', ()=> {
+//  resetGrid();
+//});
+
+function resetGrid(){
   let rows = prompt("Enter number of rows.");
   let cols = prompt("Enter number of columns.");
-//  makeRows(rows, cols);
-})
-
-function makeRows(rows, cols) {
+  if (rows > 100 || cols > 100) {
+    alert("Maximum number of rows and columns is 100.");
+  } else {
+    reset();
+    createGrid(rows, cols);
+  }
+  
+}
+function reset() {
+  document
+    .querySelectorAll(".grid-item")
+    .forEach((e) => e.parentNode.removeChild(e));
+}
+function createGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
@@ -16,4 +31,3 @@ function makeRows(rows, cols) {
   };
 };
 
-makeRows(16, 16);
